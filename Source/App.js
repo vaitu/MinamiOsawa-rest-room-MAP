08c63615d2640cf254a159700d138e6b889f5ecb
vaitu <yf.21.stf@gmail.com>
2016-07-30 03:40:31 +0900

    Update App.js

--- Source/App.js
+++ Source/App.js
@@ -1,4 +1,14 @@
 var cesiumWidget = new Cesium.Viewer('cesiumContainer');
+
+viewer.camera.setView({
+  destination: Cesium.Cartesian3.fromDegrees(139.6721684, 35.7052493, 50000),
+  orientation: {
+    heading: 0, // 水平方向の回転
+    pitch: -Cesium.Math.PI_OVER_TWO, // 垂直方向の回転 上を見上げたり下を見下ろしたり
+    roll: 0
+  }
+});
+
 viewer.dataSources.add(Cesium.KmlDataSource.load('Source/MinamiOsawa-restroom.kml'),{
          camera: viewer.scene.camera,
          canvas: viewer.scene.canvas

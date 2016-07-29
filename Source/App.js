378d1e286be4b90d7c0ba6d90dcff079effa2bd4
vaitu <yf.21.stf@gmail.com>
2016-07-30 02:40:40 +0900

    Update App.js

--- Source/App.js
+++ Source/App.js
@@ -1,5 +1,5 @@
 var cesiumWidget = new Cesium.Viewer('cesiumContainer');
-viewer.dataSources.add(Cesium.KmlDataSource.load('MinamiOsawa-rest-room-MAP/Source/MinamiOsawa-restroom.kml'),{
+viewer.dataSources.add(Cesium.KmlDataSource.load('https://vaitu.github.io/MinamiOsawa-rest-room-MAP/Source/MinamiOsawa-restroom.kml'),{
          camera: viewer.scene.camera,
          canvas: viewer.scene.canvas
 });
